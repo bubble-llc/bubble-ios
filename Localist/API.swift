f1e067a1d0e007009dd87eb7da988b780fa48c51
Steven Tran <steventt07@gmail.com>
2020-09-26 15:47:27 -0500

    added login screen

--- Localist/API.swift
+++ Localist/API.swift
@@ -1,8 +1,10 @@
 import Foundation
 
 class API {
+    let baseURL = "https://dashboard.stocksandshare.com/chitchat"
+    //let baseURL = "http://0.0.0.0:8000"
     func getPosts(completion: @escaping ([Post]) ->()){
-        guard let url = URL(string: "https://dashboard.stocksandshare.com/chitchat/feed?zipcode=78703&username=johnDoe") else {return}
+        guard let url = URL(string: "\(baseURL)/feed?zipcode=78703&username=johnDoe") else {return}
         URLSession.shared.dataTask(with: url){ (data,_,_)in
             let posts = try! JSONDecoder().decode([Post].self, from:data!)
             DispatchQueue.main.async{
@@ -15,7 +17,7 @@ class API {
     }
     
     func submitPost(submitted: [String: Any]){
-        guard let postUrl = URL(string: "https://dashboard.stocksandshare.com/chitchat/add_post_to_category") else {fatalError()}
+        guard let postUrl = URL(string: "\(baseURL)/add_post_to_category") else {fatalError()}
         
         var request = URLRequest(url: postUrl)
         request.httpMethod = "POST"
@@ -67,7 +69,7 @@ class API {
     }
     
     func createUser(submitted: [String: Any]){
-        guard let postUrl = URL(string: "https://dashboard.stocksandshare.com/chitchat/user") else {fatalError()}
+        guard let postUrl = URL(string: "\(baseURL)/user") else {fatalError()}
         
         var request = URLRequest(url: postUrl)
         request.httpMethod = "POST"
@@ -93,9 +95,74 @@ class API {
         task.resume()
     }
     
+    func validateUser(username: String, password: String) -> Bool{
+        guard let postUrl = URL(string: "\(baseURL)/user?username=\(username)&password=\(password )") else {fatalError()}
+        var result: Result<String?, NetworkError>!
+        var validUser = false
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        var request = URLRequest(url: postUrl)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = [
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        ]
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                result = .success(String(data: data, encoding: .utf8))
+            } else {
+                result = .failure(.server)
+            }
+            semaphore.signal()
+        }.resume()
+        
+        _ = semaphore.wait(wallTimeout: .distantFuture)
+        switch result {
+            case let .success(data):
+                let user = convertStringToDictionary(text: data!)?["user"]
+                let userData = user?.count
+                if userData! == 1
+                {
+                    validUser = true
+                }
+                print(userData!)
+            case let .failure(error):
+                print(error)
+            case .none:
+                print("errpr")
+            }
+
+        return validUser
+    }
 }
 
+func convertStringToDictionary(text: String) -> [String:AnyObject]? {
+    if let data = text.data(using: .utf8) {
+        do {
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:AnyObject]
+            return json
+        } catch {
+            print("Something went wrong")
+        }
+    }
+    return nil
+}
 
+extension URLResponse {
+
+    func getStatusCode() -> Int? {
+        if let httpResponse = self as? HTTPURLResponse {
+            return httpResponse.statusCode
+        }
+        return nil
+    }
+}
+
+enum NetworkError: Error {
+    case url
+    case server
+    }
 //curl --location --request POST 'http://0.0.0.0:8000/add_post_to_category' \
 //--header 'Content-Type: application/json' \
 //--data-raw '{
--- Localist/ContentView.swift
+++ Localist/ContentView.swift
@@ -2,103 +2,53 @@ import SwiftUI
 import Request
 
 struct ContentView : View {
-    @State private var subreddit: String = "swift"
-    @State private var sortBy: SortBy = .hot
-    
-    
-    
-    @State private var showSortSheet: Bool = false
-    @State private var showSubredditSheet: Bool = false
-    @State private var showCreateUser: Bool = false
-    
-    @State private var post_content: String = ""
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var status: Int = 0
+    @State var showLoginView: Bool = false
     
     var body: some View {
         NavigationView {
-            /// Load the posts
-            PostList()
-                /// Force inline `NavigationBar`
-                .navigationBarTitle(Text(""), displayMode: .inline)
-                .navigationBarItems(
-                    leading: HStack {
-                        Button(action: {self.showSubredditSheet.toggle()})
-                        {
-                            //This is where the submit button logic will go
-                            Text("New Post")
-                        }
-                    },
-                    trailing: HStack {
-//                        Button(action: {self.showSortSheet.toggle()})
-//                        {
-//                            HStack {
-//                                Image(systemName: "arrow.up.arrow.down")
-//                                Text(self.sortBy.rawValue)
-//                            }
-//                        }
-//                        Button(action: {self.showCreateUser.toggle()})
-//                        {
-//                            //This is where the submit button logic will go
-//                            Text("Create User")
-//                        }
-                        NavigationLink(destination: CreateUserView()) {
-                                            Text("Create User")
-                                        }
-
-                    }
-                )
-                /// Sorting method `ActionSheet`
-                .actionSheet(isPresented: $showSortSheet) {
-                    ActionSheet(title: Text("Sort By:"), buttons: [SortBy.hot,SortBy.new, ].map { method in
-                        ActionSheet.Button.default(Text(method.rawValue.prefix(1).uppercased() + method.rawValue.dropFirst())) {
-                            self.sortBy = method
-                        }
-                    })
-                }
-                /// Submit a post
-                .popover(isPresented: $showSubredditSheet) {
-                    VStack() {
+            VStack {
+                if showLoginView {
+                    FeedView()
+                    .navigationBarTitle(Text("Feed"))
+                } else {
+                    Form {
+                        TextField("Username", text: self.$username).autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                        SecureField("Password", text: self.$password)
                         
-                        Text("What's going on?")
-                        if #available(iOS 14.0, *)
-                        {
-                            TextEditor(text: self.$post_content)
-                                .padding()
-                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 350)
-                                .border(Color.black, width:1)
+                        if self.isUserInformationValid() {
+                            Button(action: {
+                                if API().validateUser(username: username, password:  password) == true
+                                {
+                                    self.showLoginView = true
+                                }
+                            }, label: {
+                                Text("Login")
+                            })
                         }
-                        else
-                        {
-                            // Fallback on earlier versions
-                            MultilineTextField("Enter post here...", text: self.$post_content)
-                                .padding(3)
-                                .frame(minWidth: 100, idealWidth: 100/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center)
-                                .background(RoundedRectangle(cornerRadius:5))
-                        }
-                        
-                        Button(action: {
-                            //                        self.post_content = "nice"
-                            let postObject: [String: Any]  =
-                                [
-                                    "username": "steventt07",
-                                    "category_name": "What's happening?",
-                                    "content": self.post_content,
-                                    "title": "Testing Post Feed",
-                                    "zipcode": "78703"
-                                ]
-                            API().submitPost(submitted: postObject)
-                        })
-                        {
-                            //This is where the submit button logic will go
-                            Text("Submit")
-                        }
-                    }
-                    .frame(width: 200)
-                    .padding()
-                    .background(Color("green"))
-                    .cornerRadius(10)
+                    }.navigationBarTitle(Text("Login"))
+                    
+                    NavigationLink(destination: CreateUserView()) {
+                                        Text("Create User")
+                                    }
                 }
-            Text("Select a post")
+            }
+            
         }
+    }
+    
+    private func isUserInformationValid() -> Bool {
+        if username.isEmpty {
+            return false
+        }
+        
+        if password.isEmpty {
+            return false
+        }
+        
+        return true
     }
 }
 
@@ -108,120 +58,120 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct UITextViewWrapper: UIViewRepresentable {
-    typealias UIViewType = UITextView
-
-    @Binding var text: String
-    @Binding var calculatedHeight: CGFloat
-    var onDone: (() -> Void)?
-
-    func makeUIView(context: UIViewRepresentableContext<UITextViewWrapper>) -> UITextView {
-        let textField = UITextView()
-        textField.delegate = context.coordinator
-
-        textField.isEditable = true
-        textField.font = UIFont.preferredFont(forTextStyle: .body)
-        textField.isSelectable = true
-        textField.isUserInteractionEnabled = true
-        textField.isScrollEnabled = false
-        textField.backgroundColor = UIColor.clear
-        if nil != onDone {
-            textField.returnKeyType = .done
-        }
-
-        textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        return textField
-    }
-
-    func updateUIView(_ uiView: UITextView, context: UIViewRepresentableContext<UITextViewWrapper>) {
-        if uiView.text != self.text {
-            uiView.text = self.text
-        }
-        if uiView.window != nil, !uiView.isFirstResponder {
-            uiView.becomeFirstResponder()
-        }
-        UITextViewWrapper.recalculateHeight(view: uiView, result: $calculatedHeight)
-    }
-
-    fileprivate static func recalculateHeight(view: UIView, result: Binding<CGFloat>) {
-        let newSize = view.sizeThatFits(CGSize(width: view.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
-        if result.wrappedValue != newSize.height {
-            DispatchQueue.main.async {
-                result.wrappedValue = newSize.height // !! must be called asynchronously
-            }
-        }
-    }
-
-    func makeCoordinator() -> Coordinator {
-        return Coordinator(text: $text, height: $calculatedHeight, onDone: onDone)
-    }
-
-    final class Coordinator: NSObject, UITextViewDelegate {
-        var text: Binding<String>
-        var calculatedHeight: Binding<CGFloat>
-        var onDone: (() -> Void)?
-
-        init(text: Binding<String>, height: Binding<CGFloat>, onDone: (() -> Void)? = nil) {
-            self.text = text
-            self.calculatedHeight = height
-            self.onDone = onDone
-        }
-
-        func textViewDidChange(_ uiView: UITextView) {
-            text.wrappedValue = uiView.text
-            UITextViewWrapper.recalculateHeight(view: uiView, result: calculatedHeight)
-        }
-
-        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-            if let onDone = self.onDone, text == "\n" {
-                textView.resignFirstResponder()
-                onDone()
-                return false
-            }
-            return true
-        }
-    }
-
-}
-
-struct MultilineTextField: View {
-
-    private var placeholder: String
-    private var onCommit: (() -> Void)?
-
-    @Binding private var text: String
-    private var internalText: Binding<String> {
-        Binding<String>(get: { self.text } ) {
-            self.text = $0
-            self.showingPlaceholder = $0.isEmpty
-        }
-    }
-
-    @State private var dynamicHeight: CGFloat = 100
-    @State private var showingPlaceholder = false
-
-    init (_ placeholder: String = "", text: Binding<String>, onCommit: (() -> Void)? = nil) {
-        self.placeholder = placeholder
-        self.onCommit = onCommit
-        self._text = text
-        self._showingPlaceholder = State<Bool>(initialValue: self.text.isEmpty)
-    }
-
-    var body: some View {
-        UITextViewWrapper(text: self.internalText, calculatedHeight: $dynamicHeight, onDone: onCommit)
-            .frame(minHeight: dynamicHeight, maxHeight: dynamicHeight)
-            .background(placeholderView, alignment: .topLeading)
-    }
-
-    var placeholderView: some View {
-        Group {
-            if showingPlaceholder {
-                Text(placeholder).foregroundColor(.gray)
-                    .padding(.leading, 4)
-                    .padding(.top, 8)
-            }
-        }
-    }
-}
+//struct UITextViewWrapper: UIViewRepresentable {
+//    typealias UIViewType = UITextView
+//
+//    @Binding var text: String
+//    @Binding var calculatedHeight: CGFloat
+//    var onDone: (() -> Void)?
+//
+//    func makeUIView(context: UIViewRepresentableContext<UITextViewWrapper>) -> UITextView {
+//        let textField = UITextView()
+//        textField.delegate = context.coordinator
+//
+//        textField.isEditable = true
+//        textField.font = UIFont.preferredFont(forTextStyle: .body)
+//        textField.isSelectable = true
+//        textField.isUserInteractionEnabled = true
+//        textField.isScrollEnabled = false
+//        textField.backgroundColor = UIColor.clear
+//        if nil != onDone {
+//            textField.returnKeyType = .done
+//        }
+//
+//        textField.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+//        return textField
+//    }
+//
+//    func updateUIView(_ uiView: UITextView, context: UIViewRepresentableContext<UITextViewWrapper>) {
+//        if uiView.text != self.text {
+//            uiView.text = self.text
+//        }
+//        if uiView.window != nil, !uiView.isFirstResponder {
+//            uiView.becomeFirstResponder()
+//        }
+//        UITextViewWrapper.recalculateHeight(view: uiView, result: $calculatedHeight)
+//    }
+//
+//    fileprivate static func recalculateHeight(view: UIView, result: Binding<CGFloat>) {
+//        let newSize = view.sizeThatFits(CGSize(width: view.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
+//        if result.wrappedValue != newSize.height {
+//            DispatchQueue.main.async {
+//                result.wrappedValue = newSize.height // !! must be called asynchronously
+//            }
+//        }
+//    }
+//
+//    func makeCoordinator() -> Coordinator {
+//        return Coordinator(text: $text, height: $calculatedHeight, onDone: onDone)
+//    }
+//
+//    final class Coordinator: NSObject, UITextViewDelegate {
+//        var text: Binding<String>
+//        var calculatedHeight: Binding<CGFloat>
+//        var onDone: (() -> Void)?
+//
+//        init(text: Binding<String>, height: Binding<CGFloat>, onDone: (() -> Void)? = nil) {
+//            self.text = text
+//            self.calculatedHeight = height
+//            self.onDone = onDone
+//        }
+//
+//        func textViewDidChange(_ uiView: UITextView) {
+//            text.wrappedValue = uiView.text
+//            UITextViewWrapper.recalculateHeight(view: uiView, result: calculatedHeight)
+//        }
+//
+//        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+//            if let onDone = self.onDone, text == "\n" {
+//                textView.resignFirstResponder()
+//                onDone()
+//                return false
+//            }
+//            return true
+//        }
+//    }
+//
+//}
+//
+//struct MultilineTextField: View {
+//
+//    private var placeholder: String
+//    private var onCommit: (() -> Void)?
+//
+//    @Binding private var text: String
+//    private var internalText: Binding<String> {
+//        Binding<String>(get: { self.text } ) {
+//            self.text = $0
+//            self.showingPlaceholder = $0.isEmpty
+//        }
+//    }
+//
+//    @State private var dynamicHeight: CGFloat = 100
+//    @State private var showingPlaceholder = false
+//
+//    init (_ placeholder: String = "", text: Binding<String>, onCommit: (() -> Void)? = nil) {
+//        self.placeholder = placeholder
+//        self.onCommit = onCommit
+//        self._text = text
+//        self._showingPlaceholder = State<Bool>(initialValue: self.text.isEmpty)
+//    }
+//
+//    var body: some View {
+//        UITextViewWrapper(text: self.internalText, calculatedHeight: $dynamicHeight, onDone: onCommit)
+//            .frame(minHeight: dynamicHeight, maxHeight: dynamicHeight)
+//            .background(placeholderView, alignment: .topLeading)
+//    }
+//
+//    var placeholderView: some View {
+//        Group {
+//            if showingPlaceholder {
+//                Text(placeholder).foregroundColor(.gray)
+//                    .padding(.leading, 4)
+//                    .padding(.top, 8)
+//            }
+//        }
+//    }
+//}
 
 
