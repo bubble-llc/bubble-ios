d924724727fa39b5ee5902117c0cd5a2c4474330
steventt07 <steventt07@tamu.edu>
2021-05-05 08:57:35 -0500

    changed base url

--- Bubble/API.swift
+++ Bubble/API.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 class API {
-//    fileprivate let baseURL = "https://dashboard.stocksandshare.com/chitchat"
-    fileprivate let baseURL = "http://0.0.0.0:8000"
+    fileprivate let baseURL = "https://dashboard.stocksandshare.com/chitchat"
+//    fileprivate let baseURL = "http://0.0.0.0:8000"
     let categories = ["Deals":1, "Happy Hour":2, "Recreation":3, "What's Happening?":4, "Misc":5]
     
     func getPosts(logitude: String, latitude: String, category: String, completion: @escaping (Result<[Post],Error>) ->())
