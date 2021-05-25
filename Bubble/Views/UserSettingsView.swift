c77b506301e4765880db08e936b00c1d92df3e83
Neil Pasricha <neilpasrich@gmail.com>
2021-05-25 15:00:16 -0500

    Change password form is in

--- Bubble/Views/UserSettingsView.swift
+++ Bubble/Views/UserSettingsView.swift
@@ -9,7 +9,7 @@ struct UserSettingsView: View {
     
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
-    @State private var username: String = "username"
+    @State private var username: String = " Username"
     @State private var default_post_title: String = "Location of post"
     @State private var post_title_pressed: Bool = false
     @State private var post_content: String = "Write some content for your post"
@@ -21,6 +21,12 @@ struct UserSettingsView: View {
     @State private var allNotifications = true
     @State private var commentNotifications = true
     @State private var likesNotifications = true
+    @State var changePassword : Bool = false
+    @State var showBlocked : Bool = false
+    @State private var oldPassword = ""
+    @State private var newPassword = ""
+    @State private var confirmedPassword = ""
+    
     
     @State private var deals_clicked = false
     @State private var happy_clicked = false
@@ -38,34 +44,95 @@ struct UserSettingsView: View {
             VStack(alignment: .leading){
                 Spacer()
                 Text("Account")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                     .font(.system(size: 40))
                     .foregroundColor(.white)
-                    .padding(.leading, UIScreen.main.bounds.width * 0.2)
+                    .padding(.leading, UIScreen.main.bounds.width * 0.125)
                 Spacer()
                 VStack(alignment: .leading) {
                     VStack(alignment:.leading){
                 Text("User Settings")
                     .font(.system(size: 30))
                     .foregroundColor(.white)
-                    
+                    .padding(.leading, 2.5)
+                        
                     Divider().frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.width * 0.01).background(Color("bubble_dark")).padding(-5)
                     }
-                Text("Username")
+                Text("Change Username")
                     .foregroundColor(.white)
                     .padding(.leading, UIScreen.main.bounds.width * 0.05)
+                    
                     TextEditor(text: $username)
-                    .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.033)
+                        .font(.system(size: 18))
+                        .lineLimit(0)
+                        .foregroundColor(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255))
+                        .background(Color(.white))
+                        .listRowBackground(Color(red: 112 / 255, green: 202 / 255, blue: 211 / 255))
+                        .cornerRadius(18)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 18)
+                                .stroke(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255), lineWidth: 2)
+                        )
+                    .frame(width: UIScreen.main.bounds.width * 0.4, height: UIScreen.main.bounds.height * 0.03)
                     .padding(.leading, UIScreen.main.bounds.width * 0.05)
                 HStack{//HStack containing the button to show/hide password form
-                Text("Password")
+                Text("Change Password")
                     .foregroundColor(.white)
-                Image(systemName: "eye.slash")
+                    Button(action:{
+                        self.changePassword.toggle()
+                    }){
+                    Image(systemName: self.changePassword == false ? "eye.slash": "eye")
+                    }
                 }
                 .padding(.leading, UIScreen.main.bounds.width * 0.05)
+                    if changePassword{
+                    Section{
+                        SecureField("  old password", text: $oldPassword)
+                            .font(.system(size: 18))
+                            .textContentType(.newPassword)
+                            .foregroundColor(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255))
+                            .background(Color(.white))
+                            .listRowBackground(Color(red: 112 / 255, green: 202 / 255, blue: 211 / 255))
+                            .cornerRadius(18)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255), lineWidth: 2)
+                            )
+                            
+                            .padding(.leading, UIScreen.main.bounds.width * 0.05)
+                            .frame(width: UIScreen.main.bounds.width * 0.42)
+                        SecureField("  New password", text: $newPassword)
+                            .font(.system(size: 18))
+                            .textContentType(.newPassword)
+                            .foregroundColor(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255))
+                            
+                            .background(Color(.white))
+                            .listRowBackground(Color(red: 112 / 255, green: 202 / 255, blue: 211 / 255))
+                            .cornerRadius(18)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255), lineWidth: 2)
+                            )
+                            
+                            .padding(.leading, UIScreen.main.bounds.width * 0.05)
+                            .frame(width: UIScreen.main.bounds.width * 0.42)
+                        SecureField("  New password", text: $confirmedPassword)
+                            .font(.system(size: 18))
+                            .textContentType(.newPassword)
+                            .foregroundColor(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255))
+                            .background(Color(.white))
+                            .cornerRadius(18)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 18)
+                                    .stroke(Color(red: 43 / 255, green: 149 / 255, blue: 173 / 255), lineWidth: 2)
+                            )
+                            .padding(.leading, UIScreen.main.bounds.width * 0.05)
+                            .frame(width:UIScreen.main.bounds.width * 0.42)
+                    }
+                    }
+
             }//User Settings VStack
                 .frame(width:UIScreen.main.bounds.width)
-                .padding(.leading, -UIScreen.main.bounds.width * 0.45)
+                .padding(.leading, -UIScreen.main.bounds.width * 0.47)
             Spacer()
             VStack(alignment: .leading){
                 VStack(alignment: .leading){
@@ -79,7 +146,11 @@ struct UserSettingsView: View {
                 HStack{//HStack containing the button to show/hide blocked users form
                 Text("Blocked Users")
                     .foregroundColor(.white)
-                Image(systemName: "eye.slash")
+                    Button(action:{
+                        self.showBlocked.toggle()
+                    }){
+                    Image(systemName: self.showBlocked == false ? "eye.slash": "eye")
+                    }
                 }
                 .padding(.leading, UIScreen.main.bounds.width * 0.05)
             }
@@ -96,7 +167,7 @@ struct UserSettingsView: View {
                 }
                 Toggle("All Notifications", isOn: $allNotifications)
                     .foregroundColor(.white)
-                    .toggleStyle(SwitchToggleStyle())
+                    .toggleStyle(SwitchToggleStyle(tint: Color("bubble_dark")))
                     .padding(.leading, UIScreen.main.bounds.width * 0.05)
                     .frame(width: UIScreen.main.bounds.width * 0.75)
 
@@ -106,7 +177,7 @@ struct UserSettingsView: View {
                 
                 Toggle("Comments on your posts", isOn: $commentNotifications)
                     .foregroundColor(.white)
-                    .toggleStyle(SwitchToggleStyle())
+                    .toggleStyle(SwitchToggleStyle(tint: Color("bubble_dark")))
                     .padding(.leading, UIScreen.main.bounds.width * 0.05)
                     .frame(width: UIScreen.main.bounds.width * 0.75)
 
@@ -116,7 +187,7 @@ struct UserSettingsView: View {
                 
                 Toggle("Likes on your posts", isOn: $likesNotifications)
                     .foregroundColor(.white)
-                    .toggleStyle(SwitchToggleStyle())
+                    .toggleStyle(SwitchToggleStyle(tint: Color("bubble_dark")))
                     .padding(.leading, UIScreen.main.bounds.width * 0.05)
                     .frame(width: UIScreen.main.bounds.width * 0.75)
                 if likesNotifications {
